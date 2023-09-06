22db51dea8e58c18117d3558e0c354c71223f6d7
alkorotkovv <akorotkov95@mail.ru>
2023-09-06 22:07:26 +0300

    add logic to timer

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,73 +1,84 @@
 import './Main.scss';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Main: React.FC = () => {
 
-  const [count, setCount] = useState(0);
-  const [allow, setAllow] = useState(true);
-  //const [time, setTime] = useState(1);
-  const [k, setK] = useState(0);
+const [count, setCount] = useState(0);
+const [allow, setAllow] = useState(true);
+const [k, setK] = useState(0);
+const [lastClicked, setLastClicked] = useState(Date.now());
+const [newClicked, setNewClicked] = useState(Date.now());
 
-  //let timerId: ReturnType<typeof setTimeout>;
+//const timerId: number = window.setInterval(() => console.log(k), 1000);
+//window.setInterval(() => console.log(k), 1000);
 
-  React.useEffect(() => {console.log(count)}, [count])
+const [seconds, setSeconds] = useState(1);
 
-  function toAllow() {
-    setAllow(true);
-  }
+useEffect(() => {
+  const timer = setInterval(() => {
+    setSeconds(seconds + 100);
+  }, 100);
+  return () => clearInterval(timer);
+});
 
-  function noAllow() {
-    setAllow(false);
-  }
-
-  function onTimer() {
-    console.log(count)
-    //if (count === 0)
-    //  noAllow();
-    //else toAllow();
-    console.log("время вышло");
-    //setCount(0);
-  }
-
-  let lastClicked = 0;
-
-  function onClickCheck() {
-    if (k === 0) 
-      lastClicked = (new Date()).getTime();
-      
-    
-    let timeNow = (new Date()).getTime();
-
-    if (timeNow > (lastClicked + 1000)) {
-        // Execute the link action
-        setAllow(false);
+useEffect(() => {
+  console.log("seconds " + seconds)
+  console.log("last " + lastClicked)
+  console.log("new  " + newClicked)
+  if (k) {
+    if ((Date.now() > (newClicked + 1000))) {
+      setAllow(false);
     }
     else {
-        //alert('Please wait at least 1 seconds between clicks!');
-        setAllow(true);
+      setAllow(true);
     }
+  }
+}, [seconds])
 
-    lastClicked = timeNow;
-    setK(1);
+useEffect(() => {
+  //console.log("k " + k)
+}, [k])
+
+useEffect(() => {
+  //console.log("last " + lastClicked)
+
+}, [lastClicked])
+
+useEffect(() => {
+  //console.log("new  " + newClicked)
+
+}, [newClicked])
+
+
+function onTimer() {
+  //console.log(k)
+  //console.log("last " + lastClicked)
+  //console.log("now " + newClicked)
 }
 
 
-  function handleButtonClick() {
-    if (k === 0) {
-      setTimeout(onTimer, 1000);
-    }    
-    setCount(count + 1);
-    setK(1);
-    //console.log(count);
-  };
+function handleButtonClick() {
 
-  return (
-    <main className="content">
-      <button className={allow ? "button" : "button_disables" } onClick={onClickCheck} disabled={!allow}>Кликнуть</button>
-      <p className='title'>Кликнули {count} раз</p>
-      <p className='title title_yellow'>По версии сервера 10 раз</p>
-    </main>
-  );
+  if (k === 0) {
+    setLastClicked(Date.now());
+    setNewClicked(Date.now());
+  }
+  else {
+    setLastClicked(newClicked);
+    setNewClicked(Date.now());
+  }
+
+  setCount(count + 1);
+  setK(k+1);
+}
+
+return (
+  <main className="content">
+    <button className={allow ? "button" : "button button_disabled" } onClick={handleButtonClick} disabled={!allow}>Кликнуть</button>
+    <p className='title'>Кликнули {count} раз</p>
+    <p className='title title_yellow'>По версии сервера 10 раз</p>
+  </main>
+);
 }
 
 export default Main;
